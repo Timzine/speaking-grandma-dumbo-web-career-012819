c98874f53e4e276a0ff782a3368f0ef59bbbd8c7
Lemrabt Boudadya <lboudadya@gmail.com>
2018-12-22 19:59:16 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,8 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(string)
-  
-  
+  if string == string.downcase
+    
+  then return 
 end
 
 # Whatever you say to grandma, she should respond with
