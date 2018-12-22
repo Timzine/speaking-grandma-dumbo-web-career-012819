89dd7e757bcf493cb3c421e635e2144dca861754
Lemrabt Boudadya <lboudadya@gmail.com>
2018-12-22 20:18:31 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,14 +3,14 @@ def speak_to_grandma(string)
   if 
       string == string.downcase
   
-      puts "HUH?! SPEAK UP, SONNY!"
+      return "HUH?! SPEAK UP, SONNY!"
   elsif 
        string == string.upcase
   
-       puts "NO, NOT SINCE 1938!"
+       return "NO, NOT SINCE 1938!"
   else
        string == "I LOVE YOU GRANDMA!"
-       puts "I LOVE YOU TOO PUMPKIN!"
+       return "I LOVE YOU TOO PUMPKIN!"
 end
 
 # Whatever you say to grandma, she should respond with
