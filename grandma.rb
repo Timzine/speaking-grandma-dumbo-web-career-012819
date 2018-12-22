c3481611edc89ab068025ec200ac6376c2c8608d
Lemrabt Boudadya <lboudadya@gmail.com>
2018-12-22 20:27:02 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,20 +1,17 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(string)
   if 
-     string == string.downcase
-      
+      string == string.downcase
   
       return "HUH?! SPEAK UP, SONNY!"
   elsif 
-        string == string.upcase
-       
+       string == string.upcase
   
        return "NO, NOT SINCE 1938!"
   else
-        string == "I LOVE YOU GRANDMA!"
-       
+       string == "I LOVE YOU GRANDMA!"
        return "I LOVE YOU TOO PUMPKIN!"
-  end     
+end    
        
 end
 
