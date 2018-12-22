182f627f375ad0956f4fb2d78820b8520e85054a
Lemrabt Boudadya <lboudadya@gmail.com>
2018-12-22 20:05:08 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -9,8 +9,8 @@ def speak_to_grandma(string)
   then
        return "NO, NOT SINCE 1938!"
   else
-       string == ""
-     
+       string == "I LOVE YOU GRANDMA!"
+     return ""
 end
 
 # Whatever you say to grandma, she should respond with
